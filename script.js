c30eb09597410e5b054a700a9bedf5eea4b89a44
Thuan1110 <67035930+Thuan1110@users.noreply.github.com>
2023-03-27 10:15:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+backToTopButton.addEventListener("click", scrollToTop);
